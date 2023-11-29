8c0b8bcef35c61275e7725edc6ce29bccc4e0063
Kouadio Fabrice Nguessan <72697416+fabnguess@users.noreply.github.com>
2023-11-29 09:13:50 +0000

    refactor(test): Migrating from tape to Node.js test runner (#52)

--- tests/licenses.test.js
+++ tests/licenses.test.js
@@ -1,21 +1,20 @@
-// Import Third-party Dependencies
-import test from "tape";
+// Import Node.js Dependencies
+import { describe, test } from "node:test";
+import assert from "node:assert";
 
 // Import Internal Dependencies
 import { closestSpdxLicenseID } from "../src/licenses.js";
 
-// Check everytruthy
-test("it should return the given LicenseID if no record match", (tape) => {
-  tape.same(closestSpdxLicenseID("foooobar"), "foooobar");
-  tape.end();
-});
+describe("Check everytruthy", () => {
+  test("it should return the given LicenseID if no record match", () => {
+    assert.equal(closestSpdxLicenseID("foooobar"), "foooobar");
+  });
 
-test("it should fix 'BSD 3-Clause' to 'BSD-3-Clause'", (tape) => {
-  tape.same(closestSpdxLicenseID("BSD 3-Clause"), "BSD-3-Clause");
-  tape.end();
-});
+  test("it should fix 'BSD 3-Clause' to 'BSD-3-Clause'", () => {
+    assert.equal(closestSpdxLicenseID("BSD 3-Clause"), "BSD-3-Clause");
+  });
 
-test("it should not fix 'BSD 3 Clause' because the distance is greater than one", (tape) => {
-  tape.same(closestSpdxLicenseID("BSD 3 Clause"), "BSD 3 Clause");
-  tape.end();
+  test("it should not fix 'BSD 3 Clause' because the distance is greater than one", () => {
+    assert.equal(closestSpdxLicenseID("BSD 3 Clause"), "BSD 3 Clause");
+  });
 });
